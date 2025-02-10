f699110c50feb45ba81ecc43634dbfcda5061d9f
sky <akash.ap111@outlook.com>
2025-02-10 13:38:19 +0530

    static

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  basePath: "/skysoul159.github.io",
   images: {
+    unoptimized: true,
     domains: [
       "images.unsplash.com",
       "assets.aceternity.com",

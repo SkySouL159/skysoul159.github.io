c3427114ef3fa85f7e70d356bbd6cf7a73544e4b
sky <akash.ap111@outlook.com>
2025-02-12 14:05:05 +0530

    remove unused image and video assets

---
+++ app/page.js
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <>
+      <navbar>
+        <div className="logo">
+          <Image src="/globe.svg" alt="logo" width="50" height="50" />
+        </div>
+        <div className="links">
+          <Link href="/portfolio">Portfolio</Link>
+          <Link href="/about">About</Link>
+          <Link href="/contact">Contact</Link>
+        </div>
+      </navbar>
+    </>
+  );
+}

0001233cced186c854e592801f15ba2691fe2a1b
sky <akash.ap111@outlook.com>
2025-02-13 20:00:52 +0530

    update next.config.mjs for GitHub Pages deployment and add GitHub Actions workflow

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,12 @@
-/** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
-  output: "export",
-  assetPrefix:
-    process.env.NODE_ENV === "production" ? "/skysoul159.github.io/" : "",
-  basePath:
-    process.env.NODE_ENV === "production" ? "/skysoul159.github.io" : "",
-
+  output: "export", // Required for static export
+  basePath: isProd ? "/skysoul159.github.io" : "", // Set the repo name
+  assetPrefix: isProd ? "/skysoul159.github.io/" : "",
   images: {
-    unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "skysoul159.github.io",
-        pathname: "/**",
-      },
-    ],
+    unoptimized: true, // Required for GitHub Pages
   },
-  trailingSlash: true,
 };
 
-export default nextConfig;
+module.exports = nextConfig;

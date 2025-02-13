efbbebc0b281c7351564ce41b2ce1e92a4147611
sky <akash.ap111@outlook.com>
2025-02-13 19:27:11 +0530

    remove deploy script from package.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath:
-    process.env.NODE_ENV === "production" ? "/skysoul159.github.io" : "",
-  assetPrefix:
-    process.env.NODE_ENV === "production" ? "/skysoul159.github.io" : "",
+
   images: {
     unoptimized: true,
     remotePatterns: [
